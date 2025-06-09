8eab32cab55f00bc727a62e134f67ed2e11120ce
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-09 11:10:18 -0400

    Added function names and README.md for Workload_2

---
+++ workloads/workload_2/workload_2.py
@@ -0,0 +1,18 @@
+class Workload:
+    """Class definition for handling the workload's functions."""
+
+    def __init__(self):
+        """Initializes an instance from the Workload class."""
+        pass
+
+    def generate_random(self):
+        """Picks a random integer from a range of integers."""
+        pass
+
+    def run(self):
+        """Runs the workload."""
+        pass
+
+    def process_output(self):
+        """Process the test output to determine whether the test passed or failed."""
+        pass
