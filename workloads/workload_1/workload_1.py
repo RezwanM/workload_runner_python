dcf1591e94a7e78c8560f72a08de075c4ceea4a2
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-09 11:26:59 -0400

    Added code for the generate_random function in Workload_1

--- workloads/workload_1/workload_1.py
+++ workloads/workload_1/workload_1.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Workload:
     """Class definition for handling the workload's functions."""
 
@@ -5,9 +8,20 @@ class Workload:
         """Initializes an instance from the Workload class."""
         pass
 
-    def generate_random(self):
-        """Picks a random integer from a range of integers."""
-        pass
+    def generate_random(self, low: int, high: int, seed: int) -> int:
+        """Picks a random integer from a range of integers.
+
+        Args:
+            low: The lowest integer in the range.
+            high: The highest integer in the range.
+            seed: The input seed for the bit generator.
+
+        Returns:
+            The random integer.
+        """
+        rng = np.random.default_rng(seed=seed)
+        self.number = rng.integers(low=low, high=high, size=1)
+        return self.number[0]
 
     def run(self):
         """Runs the workload."""
