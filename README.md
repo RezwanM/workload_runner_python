89020ab0cea81672678b28fce4657f8219bbc37f
Rezwan Matin <rezwanmatin@gmail.com>
2025-06-10 10:47:50 -0400

    Added code in main.py and updated README.md with example command-lines

--- README.md
+++ README.md
@@ -1,5 +1,76 @@
-# workload_runner_python
+# Workload-Runner
 
 The goal of this project was to create a validation tool that can automatically create and execute random, reproducible, and concurrent tests once launched. The tool - *Workload-Runner* - is meant to be accompanied by multiple OS applications (i.e., workloads) which can be used to stress the different IPs of the system. The project was intended to provide a glimpse of the true power of an automated validation framework, and for that reason it employs very simple workloads for demonstration purposes.  
 
-This project was written in Python and Bash.
+## Preview
+
+    $ python3 ./main.py -wl Workload_1 -wl Workload_2 -iter 2 -seed 123
+    ########################################################################################################################
+    --------------------------------------------  W o r k l o a d - R u n n e r  -------------------------------------------
+    ########################################################################################################################
+    ---------------------------------------------------  by Rezwan Matin  --------------------------------------------------
+    ---------------------------------------------------  Copyright 2025  ---------------------------------------------------
+    ########################################################################################################################
+                                                          TEST STARTED
+                                                         Now running...
+                                                         > Workload_1 <
+                                                         > Workload_2 <
+                                              Test started with a user-defined seed
+                                             Test will run based on input iterations
+    ########################################################################################################################
+                                                            Seed: 123
+                                                        Iteration 1 of 2
+                                                         1 wl_1: PASSED!
+                                                         1 wl_2: PASSED!
+    ########################################################################################################################
+                                                            Seed: 113
+                                                        Iteration 2 of 2
+                                                         2 wl_1: PASSED!
+                                                         2 wl_2: PASSED!
+    ########################################################################################################################
+                                                           TEST ENDED
+                                                   Total runtime: 1.16 seconds
+                                                       Total iterations: 2
+                                                         Total tasks: 4
+                                                            Passed: 4
+                                                            Failed: 0
+    ########################################################################################################################
+
+## Requirements
+
+- Python 3.12.9
+- Numpy 2.2.3
+
+## Options
+
+| Flag               | Description                                                 |
+| ------------------ | ----------------------------------------------------------- |
+| -h, --help         | Display the help information on how to use Workload-Runner. |
+| -wl, --workload    | The workload(s) selected for the regression test.           |
+| -iter, --iteration | The number of loops the test should run.                    |
+| -time, --seed      | The regression runtime (in seconds).                        |
+| -seed, --seed      | The input seed.                                             |
+
+**Note:** Workload-Runner requires either iterations or runtime for setting the regression test duration. If both are undefined at input, Workload-Runner will default to running a single iteration of the test.
+
+## Usage
+
+    python3 ./main.py -wl <workload_#1> -wl <workload_#2> ... -iter <iterations> -time <runtime> -seed <seed>
+
+## Examples
+
+### Running *Workload_1* with a random seed for a single itertion (default)
+
+    python3 ./main.py -wl Workload_1
+
+### Running *Workload_1* with a random seed for 3 itertions
+
+    python3 ./main.py -wl Workload_1 -iter 3
+
+### Running *Workload_2* with input runtime and random seed
+
+    python3 ./main.py -wl Workload_2 -time 30
+
+### Running *Workload_1* and *Workload_2* with input iterations and input seed
+
+    python3 ./main.py -wl Workload_1 -wl Workload_2 -iter 5 -seed 123
---
+++ main.py
@@ -0,0 +1,150 @@
+"""This is the script which launches Workload-Runner.
+
+Please see the README.md file for example command-lines.
+"""
+
+import importlib
+from typing import Tuple
+
+from utils.args import Args
+from utils.logger import Logger
+from utils.parallel import Parallel
+from utils.paths import Paths
+from utils.randomizer import Randomizer
+from utils.timer import Timer
+
+
+def main():
+    args_parser = Args()
+    logger = Logger()
+    parallel = Parallel()
+    paths = Paths()
+    randomizer = Randomizer()
+    timer = Timer()
+    args = args_parser.parse_arguments()
+    bool_args = args_parser.get_bool_args(args)
+    bins_path = paths.bins_path
+    wl_module = {}
+    wl_config = {}
+    args.workload = set(args.workload)
+    for wl in args.workload:
+        wl_module_str = paths.get_wl_module(workload=wl)
+        wl_module[wl] = importlib.import_module(wl_module_str).Workload()
+        wl_config_str = paths.get_wl_config(workload=wl)
+        wl_config[wl] = importlib.import_module(wl_config_str)
+    logger.run_pre_exec(
+        wl_list=args.workload,
+        is_iter=bool_args["is_iter"],
+        is_random=bool_args["is_random"],
+    )
+    if bool_args["is_random"]:
+        start_seed = randomizer.generate_seed()
+    else:
+        start_seed = args.seed
+    current_seed = start_seed
+    pass_count = 0
+    task_count = 0
+    start_time = timer.press_timer()
+
+    def loop_execution(
+        current_seed: int, iter_id: int, task_count: int, pass_count: int
+    ) -> Tuple[int, int, int, int]:
+        """Execute steps for running a regression test for the given input iterations/runtime.
+
+        Args:
+            current_seed: The current iteration seed.
+            iter_id: The current iteration ID.
+            task_count: Counter for recording the number of tasks run.
+            pass_count: Counter for recording the number of tasks that passed.
+
+        Returns:
+            The updated input arguments.
+
+        Raises:
+            FileNotFoundError: If output log file is not found.
+        """
+        log_dict = {}
+        attributes_dict = {}
+        for wl in args.workload:
+            attributes_dict[wl] = wl_config[wl].attributes[
+                randomizer.pick_random_int(
+                    low=0, high=len(wl_config[wl].attributes), seed=current_seed
+                )
+            ]
+            subdir_path, log_paths = logger.run_exec(
+                seed=current_seed, workload=wl, current_iter=iter_id
+            )
+            log_dict[wl] = log_paths[0]
+            parallel.start_process(
+                func=wl_module[wl].run(
+                    seed=current_seed,
+                    bin_path=wl_config[wl].bin_path[attributes_dict[wl]],
+                    stdout_path=log_paths[0],
+                    stderr_path=log_paths[1],
+                )
+            )
+        parallel.wait()
+        for wl, log_path in log_dict.items():
+            try:
+                is_pass = wl_module[wl].process_output(stdout_path=log_path)
+            except FileNotFoundError:
+                logger.print_error("Couldn't read output file!")
+            pass_count += logger.run_post_exec(
+                is_pass=is_pass,
+                log_paths=log_paths,
+                attribute=attributes_dict[wl],
+                iter_id=iter_id,
+            )
+            task_count += 1
+        current_seed = randomizer.generate_seed_from_seed(seed=current_seed)
+        return current_seed, iter_id, task_count, pass_count
+
+    if bool_args["is_iter"]:
+        for iter_id in range(1, args.iteration + 1):
+            logger.print_iter(
+                current_iter=iter_id, total_iter=args.iteration, seed=current_seed
+            )
+            current_seed, iter_id, task_count, pass_count = loop_execution(
+                current_seed=current_seed,
+                iter_id=iter_id,
+                task_count=task_count,
+                pass_count=pass_count,
+            )
+    else:
+        iter_id = 0
+        iter_time = 0
+        while (
+            timer.is_time_remaining
+            and timer.time_remaining(start_time=start_time, end_time=args.time)
+            > iter_time
+        ):
+            iter_id += 1
+            iter_start_time = timer.press_timer()
+            logger.print_time(
+                time_left=timer.time_remaining(
+                    start_time=start_time, end_time=args.time
+                ),
+                total_time=args.time,
+                seed=current_seed,
+            )
+            current_seed, iter_id, task_count, pass_count = loop_execution(
+                current_seed=current_seed,
+                iter_id=iter_id,
+                task_count=task_count,
+                pass_count=pass_count,
+            )
+            iter_end_time = timer.press_timer()
+            iter_time = iter_end_time - iter_start_time
+    end_time = timer.press_timer()
+    total_time = end_time - start_time
+    total_iter = iter_id
+    logger.print_summary(
+        pass_count=pass_count,
+        task_count=task_count,
+        total_iter=total_iter,
+        total_time=total_time,
+    )
+
+
+if __name__ == "__main__":
+    main()
